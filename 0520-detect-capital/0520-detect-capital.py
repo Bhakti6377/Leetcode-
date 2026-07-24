ff50248a4a78a6edbf28d73a560cce76b5d137d7
Bhakti Yashawant <bhaktiyashawant733@gmail.com>
2026-07-24 19:05:34 +0530

    Time: 0 ms (100%), Space: 19.3 MB (23.48%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,14 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+
+        if word == word.upper():
+            return True
+
+        if word == word.lower():
+            return True
+
+        if word[0].isupper() and word[1:] == word[1:].lower():
+            return True
+
+        return False
+        
